87a7819b75f08d4c178fd4f4f9ac89720167427b
Quinn_F <xoxo_x@126.com>
2017-10-17 10:47:13 +0800

    dd

--- XZPictureLookUpVc.swift
+++ XZPictureLookUpVc.swift
@@ -11,44 +11,44 @@ import UIKit
 class XZPictureLookUpView: UIView , UIScrollViewDelegate {
     
     /// 记录原始位置信息
-    private var originFrame : CGRect!
+    fileprivate var originFrame : CGRect!
     /// 缩放大小，暂时未用到
-    private var scale : CGFloat!
+    fileprivate var scale : CGFloat!
     /// 图片
     var image : UIImage!
     /// 图片imageview
-    private var xzImageView : UIImageView!
+    fileprivate var xzImageView : UIImageView!
     /// 背景视图
-    private var xzBackgroundView : UIView!
+    fileprivate var xzBackgroundView : UIView!
     /// 图片查看scrollview
-    private var xzScrollView : UIScrollView!
+    fileprivate var xzScrollView : UIScrollView!
     
     /// 展示回调事件
-    var sureAction:(parament:AnyObject,image:UIImage)->() = ({_ in
+    var sureAction:(_ parament:AnyObject,_ image:UIImage)->() = ({_ in
         
     })
     /// 关闭回调事件
-    var cancelAction:(parament:AnyObject,image:UIImage)->() = ({_ in
+    var cancelAction:(_ parament:AnyObject,_ image:UIImage)->() = ({_ in
         
     })
     
     static func creat() -> XZPictureLookUpView
     {
         let view = XZPictureLookUpView()
-        view.frame = UIScreen.mainScreen().bounds
+        view.frame = UIScreen.main.bounds
         return view
     }
     
     //MARK: - 背景点击事件
-    func backgroundTap(sender:AnyObject) {
+    func backgroundTap(_ sender:AnyObject) {
         
         close {
-            self.cancelAction(parament: "",image: UIImage())
+            self.cancelAction("" as AnyObject,UIImage())
         }
     }
     
     //MARK: - 关闭查看器
-    func close(complete:()->()) {
+    func close(_ complete:@escaping ()->()) {
         
         colseAnimation {
             print(self.cancelAction)
@@ -58,7 +58,7 @@ class XZPictureLookUpView: UIView , UIScrollViewDelegate {
     }
     
     //MARK: - 显示查看器
-    func showWithImage(image:UIImage,frame:CGRect,fromView:UIView)  {
+    func showWithImage(_ image:UIImage,frame:CGRect,fromView:UIView)  {
         
         /**
          配置imageview
@@ -66,10 +66,10 @@ class XZPictureLookUpView: UIView , UIScrollViewDelegate {
          - parameter frame: xzImageView
          */
         xzImageView = UIImageView.init(frame: self.bounds)
-        xzImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        xzImageView.contentMode = UIViewContentMode.scaleAspectFit
         xzImageView.image = image
-        xzImageView.frame = self.convertRect(frame, fromView: fromView)
-        originFrame = self.convertRect(frame, fromView: fromView)
+        xzImageView.frame = self.convert(frame, from: fromView)
+        originFrame = self.convert(frame, from: fromView)
         /**
          配置backgroudview
          
@@ -77,7 +77,7 @@ class XZPictureLookUpView: UIView , UIScrollViewDelegate {
          */
         xzBackgroundView = UIView.init(frame: self.bounds)
         xzBackgroundView.backgroundColor = colorFromHex("000000")
-        self.insertSubview(xzBackgroundView, atIndex: 0)
+        self.insertSubview(xzBackgroundView, at: 0)
         let tap = UITapGestureRecognizer.init(target: self, action: #selector(self.backgroundTap(_:)))
         self.addGestureRecognizer(tap)
         
@@ -87,68 +87,106 @@ class XZPictureLookUpView: UIView , UIScrollViewDelegate {
          - parameter frame: xzScrollView
          */
         xzScrollView = UIScrollView.init(frame: self.bounds)
-        xzScrollView.contentSize = self.bounds.size
+        xzScrollView.contentSize = xzImageView.size()
+        xzScrollView.delegate = self
         xzScrollView.zoomScale = 1
-        xzScrollView.maximumZoomScale = 2
+        xzScrollView.maximumZoomScale = 2.0
         xzScrollView.minimumZoomScale = 0.5
+        xzScrollView.isScrollEnabled = true;
+
         xzScrollView.delegate = self
         xzScrollView.addSubview(xzImageView)
         
         self.addSubview(xzScrollView)
         
-        UIApplication.sharedApplication().keyWindow?.addSubview(self)
+        UIApplication.shared.keyWindow?.addSubview(self)
         
         showAnimation()
-        sureAction(parament: "",image: image)
+        sureAction("" as AnyObject,image)
     }
+    
+
     
     /**
      close效果
      */
-    func colseAnimation(complete:()->()) {
+    func colseAnimation(_ complete:@escaping ()->()) {
+        //        let ani = CABasicAnimation()
+        //        ani.duration = 0.23
+        //        ani.removedOnCompletion = true
+        //        ani.keyPath = "transform.scale"
+        //        ani.fromValue = NSNumber.init(float: 1)
+        //        ani.toValue = NSNumber.init(float: 0.01)
+        //
+        //        self.layer.addAnimation(ani, forKey: "close")
         
-        UIView.animateWithDuration(0.3, animations: {
-            self.xzImageView.transform = CGAffineTransformIdentity
+        UIView.animate(withDuration: 0.3, animations: {
+            self.xzImageView.transform = CGAffineTransform.identity
             self.xzImageView.frame = self.originFrame
             self.xzBackgroundView.alpha = 0
-        }) { (aa) in
+        }, completion: { (aa) in
             if aa{
                 complete()
             }
-        }
+        }) 
     }
     
     /**
      show效果
      */
     func showAnimation() {
-     
-        UIView.animateWithDuration(0.3) {
-            self.xzImageView.transform = CGAffineTransformIdentity
+        //        let ani = CABasicAnimation()
+        //        ani.duration = 0.23
+        //        ani.removedOnCompletion = false
+        //        ani.keyPath = "transform.scale"
+        //        ani.fromValue = NSNumber.init(float: 1)
+        //        ani.toValue = NSNumber.init(float: Float(scale))
+        
+        //        let traslationx = CABasicAnimation()
+        //        traslationx.duration = 0.23
+        //        traslationx.keyPath = "transform.translation.x"
+        //        traslationx.fromValue = NSNumber.init(float: Float(xzScrollView.center.x))
+        //        traslationx.fromValue = NSNumber.init(float: Float(self.center.x))
+        //
+        //        let traslationy = CABasicAnimation()
+        //        traslationy.duration = 0.23
+        //        traslationy.keyPath = "transform.translation.y"
+        //        traslationy.fromValue = NSNumber.init(float: Float(xzScrollView.center.y))
+        //        traslationy.fromValue = NSNumber.init(float: Float(self.center.y))
+        //
+        //        let group = CAAnimationGroup()
+        //        group.animations = [ani,traslationx,traslationy]
+        
+        //        xzImageView.layer.addAnimation(ani, forKey: "show")
+        UIView.animate(withDuration: 0.3, animations: {
+            self.xzImageView.transform = CGAffineTransform.identity
             let size = ((self.xzImageView.image) != nil) ? self.xzImageView.image?.size : self.xzImageView.frame.size
-            let ratio = min(UIScreen.mainScreen().bounds.size.width / (size?.width)!, UIScreen.mainScreen().bounds.size.height / (size?.height)!)
+            let ratio = min(UIScreen.main.bounds.size.width / (size?.width)!, UIScreen.main.bounds.size.height / (size?.height)!)
             let w = ratio * (size?.width)!
             let h = ratio * (size?.height)!
-            self.xzImageView.frame = CGRectMake((UIScreen.mainScreen().bounds.size.width - w) / 2, (UIScreen.mainScreen().bounds.size.height - w) / 2, w, h)
-        }
+            self.xzImageView.frame = CGRect(x: (UIScreen.main.bounds.size.width - w) / 2, y: (UIScreen.main.bounds.size.height - w) / 2, width: w, height: h)
+        }) 
  
     }
+    //MARK: - 代理
     
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return xzImageView
     }
     
-    func scrollViewDidZoom(scrollView: UIScrollView) {
-        
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
     }
     
-    func scrollViewWillBeginZooming(scrollView: UIScrollView, withView view: UIView?) {
-        
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        xzImageView.center = scrollView.center
     }
     
-    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
-        
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollView.zoomScale = 1
     }
     
     
